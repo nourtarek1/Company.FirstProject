c9d5a69785d0fdcec45db50b97a7425f8e206a90
nourtarek <nourtarek541@gmaul.com>
2025-03-10 01:36:08 +0200

    Department Module DAL & BLL

---
+++ Company.FirstProject.BLL/Repositoris/DepartmentRepository.cs
@@ -0,0 +1,45 @@
+﻿using Company.FirstProject.BLL.Interfaces;
+using Company.FirstProject.DAL.Data.Context;
+using Company.FirstProject.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.FirstProject.BLL.Repositoris
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly CompanyDbContext _contex;
+
+        public DepartmentRepository()
+        {
+            _contex = new CompanyDbContext();
+        }
+        public IEnumerable<Department> GetAll()
+        {
+            return _contex.Departments.ToList();
+        }
+        public Department? Get(int id)
+        {
+            return _contex.Departments.Find(id);
+        }
+
+        public int Add(Department model)
+        {
+            _contex.Departments.Add(model);
+            return _contex.SaveChanges();
+        }
+        public int Update(Department model)
+        {
+            _contex.Departments.Update(model);
+            return _contex.SaveChanges();
+        }
+        public int Delete(Department model)
+        {
+            _contex.Departments.Remove(model);
+            return _contex.SaveChanges();
+        }
+    }
+}
---
+++ Company.FirstProject.DAL/Data/Configrations/DepartmentConfigruations.cs
@@ -0,0 +1,19 @@
+﻿using Company.FirstProject.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.FirstProject.DAL.Data.Configrations
+{
+    public class DepartmentConfigruations : IEntityTypeConfiguration<Department>
+    {
+        void IEntityTypeConfiguration<Department>.Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(1,1);
+        }
+    }
+}
